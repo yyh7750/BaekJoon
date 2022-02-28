a7375390555c5b068781666644129b929dcc2b2e
Younghun Yu <66967395+yyh7750@users.noreply.github.com>
2022-02-28 15:50:35 +0900

    [Bronze V] Title: 개, Time: 120 ms, Memory: 13840 KB -BaekjoonHub

---
+++ 백준/Bronze/10172.개/개.java
@@ -0,0 +1,15 @@
+public class Main {
+
+	/**
+	 * @methodName : main
+	 * @description : 
+	 * @param args
+	 *
+	 * @author : Younghun Yu
+	 * @date : 2022.02.28
+	 */
+	public static void main(String[] args) {
+		System.out.println("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+	}
+
+}
