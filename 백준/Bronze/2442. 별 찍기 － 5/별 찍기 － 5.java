2625f0fc68da31a8cf68d4de6ccee146820ff85f
Younghun Yoo <66967395+yyh7750@users.noreply.github.com>
2022-07-23 13:32:28 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 372 ms, Memory: 20724 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			for (int j = i; j < N - 1; j++) {
+				System.out.print(" ");
+			}
+			for (int j = N; j <= N + i * 2; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
