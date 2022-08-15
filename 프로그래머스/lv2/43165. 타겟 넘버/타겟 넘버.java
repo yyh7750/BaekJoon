b6c6870d06658dd00e671aaddaa9182f6cc43727
Younghun Yoo <66967395+yyh7750@users.noreply.github.com>
2022-08-15 22:03:24 +0900

    [level 2] Title: 타겟 넘버, Time: 0.44 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,24 @@
+public class Solution {
+
+	private static int answer;
+
+	public int solution(int[] numbers, int target) {
+		answer = 0;
+
+		dfs(0, 0, numbers, target);
+
+		return answer;
+	}
+
+	private void dfs(int idx, int sum, int[] numbers, int target) {
+		if (idx == numbers.length) {
+			if (sum == target) {
+				answer++;
+			}
+			return;
+		}
+
+		dfs(idx + 1, sum + numbers[idx], numbers, target);
+		dfs(idx + 1, sum - numbers[idx], numbers, target);
+	}
+}
