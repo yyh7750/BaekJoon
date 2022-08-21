7b88dae7b1f98eba169a7892e8e8c948e4a39a14
Younghun Yoo <66967395+yyh7750@users.noreply.github.com>
2022-08-22 01:11:57 +0900

    [Silver III] Title: 바이러스, Time: 76 ms, Memory: 11640 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/README.md
+++ 백준/Silver/2606. 바이러스/README.md
@@ -4,11 +4,11 @@
 
 ### 성능 요약
 
-메모리: 14276 KB, 시간: 124 ms
+메모리: 11640 KB, 시간: 76 ms
 
 ### 분류
 
-너비 우선 탐색(bfs), 깊이 우선 탐색(dfs), 그래프 이론(graphs), 그래프 탐색(graph_traversal)
+그래프 이론(graphs), 그래프 탐색(graph_traversal), 너비 우선 탐색(bfs), 깊이 우선 탐색(dfs)
 
 ### 문제 설명
 
--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -4,67 +4,50 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	private static int result = 0;
 
-	public static void main(String[] args) {
+	private static int result;
+	private static int[][] adjArr;
+	private static boolean[] visited;
 
-		BufferedReader br = null;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
-		// 컴퓨터 수 - 정점
-		int n = 0;
-		// 이어져 있는 쌍의 수 - 간선
+		int N = 0;
 		int pair = 0;
 
-		// 그래프 - 인접행렬
-		int[][] graph = null;
-		// 방문여부
-		boolean[] visited = null;
 
-		try {
+		N = Integer.parseInt(br.readLine());
+		pair = Integer.parseInt(br.readLine());
 
-			br = new BufferedReader(new InputStreamReader(System.in));
-			n = Integer.parseInt(br.readLine());
-			pair = Integer.parseInt(br.readLine());
+		adjArr = new int[N + 1][N + 1];
+		visited = new boolean[N + 1];
 
-			graph = new int[n + 1][n + 1];
-			visited = new boolean[n + 1];
-			
-			// 인접행렬 생성 - 간선 연결정보 저장
-			for(int i = 0; i < pair; i++) {
-				st = new StringTokenizer(br.readLine());
-				int line1 = Integer.parseInt(st.nextToken());
-				int line2 = Integer.parseInt(st.nextToken());
-				
-				graph[line1][line2] = 1;
-				graph[line2][line1] = 1;
-			}
-			
-			dfs(graph, visited, 1);
-			
-			// 1번 컴퓨터 제외
-			System.out.println(result - 1);
+		for (int i = 0; i < pair; i++) {
+			st = new StringTokenizer(br.readLine());
+			int line1 = Integer.parseInt(st.nextToken());
+			int line2 = Integer.parseInt(st.nextToken());
 
-		} //
-		catch (IOException ioException) {
-			System.out.println("입력 오류. 다시 실행해주세요.");
-		} //
-		catch (NumberFormatException numberException) {
-			System.out.println("숫자를 입력해주세요. 다시 실행해주세요.");
+			adjArr[line1][line2] = 1;
+			adjArr[line2][line1] = 1;
 		}
+
+		dfs(adjArr, visited, 1);
+
+		// 1번 컴퓨터 제외
+		System.out.println(result - 1);
 	}
-	
+
 	private static void dfs(int[][] graph, boolean[] visited, int n) {
-		// 재귀함수 종료 조건 설정
-		if(visited[n]) {
+		if (visited[n]) {
 			return;
 		}
-		
+
 		visited[n] = true;
 		result++;
-		// 탐색 시작
-		for(int i = 0; i < graph[n].length; i++) {
-			if(graph[n][i] == 1 && !visited[i]) {
+
+		for (int i = 0; i < graph[n].length; i++) {
+			if (graph[n][i] == 1 && !visited[i]) {
 				dfs(graph, visited, i);
 			}
 		}
