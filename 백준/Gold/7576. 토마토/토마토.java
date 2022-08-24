77d74b7c32e86e54524e71b67722bd5f1eda54a7
Younghun Yoo <66967395+yyh7750@users.noreply.github.com>
2022-08-24 20:55:02 +0900

    [Gold V] Title: 토마토, Time: 568 ms, Memory: 138868 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,100 +1,82 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	private static int[][] map = null;
+	private static int[][] map;
+	private static int M, N, cnt;
+	private static int[] dr = { -1, 1, 0, 0 };
+	private static int[] dc = { 0, 0, -1, 1 };
+	private static List<int[]> tomato;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		BufferedReader br = null;
-		StringTokenizer st = null;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
 
-		int n = 0;
-		int m = 0;
-
-		try {
-
-			br = new BufferedReader(new InputStreamReader(System.in));
+		map = new int[N][M];
+		tomato = new ArrayList<>();
+		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-
-			// 토마토 밭 크기 입력
-			n = Integer.parseInt(st.nextToken());
-			m = Integer.parseInt(st.nextToken());
-
-			map = new int[m][n];
-
-			// 토마토 밭 입력
-			for (int i = 0; i < m; i++) {
-				st = new StringTokenizer(br.readLine());
-				for (int j = 0; j < n; j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());
+			for (int j = 0; j < M; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if (map[i][j] == 1) {
+					tomato.add(new int[] { i, j });
 				}
 			}
+		}
 
-			int result = bfs();
+		bfs();
 
-			System.out.println(result);
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if(map[i][j] == 0) {
+					System.out.println(-1);
+					return;
+				}
+			}
+		}
+		
+		System.out.println(cnt - 1);
+	}
 
-		} //
-		catch (IOException ioException) {
-			System.out.println("입력 오류. 다시 실행해주세요.");
-		} //
-		catch (NumberFormatException numberException) {
-			System.out.println("숫자를 입력해주세요. 다시 실행해주세요.");
+	private static void bfs() {
+		Queue<int[]> Q = new LinkedList<>();
+		for (int i = 0; i < tomato.size(); i++) {
+			Q.offer(new int[] { tomato.get(i)[0], tomato.get(i)[1] });
+		}
+
+		while (!Q.isEmpty()) {
+			int qSize = Q.size();
+
+			for (int i = 0; i < qSize; i++) {
+				int[] pollLoc = Q.poll();
+				for (int d = 0; d < 4; d++) {
+					int nr = pollLoc[0] + dr[d];
+					int nc = pollLoc[1] + dc[d];
+
+					if (isChecked(nr, nc) && map[nr][nc] == 0) {
+						Q.offer(new int[] { nr, nc });
+						map[nr][nc] = 1;
+					}
+				}
+			}
+			cnt++;
 		}
 	}
 
-	private static int bfs() {
-		// 상하
-		int[] dx = { -1, 1, 0, 0 };
-		// 좌우
-		int[] dy = { 0, 0, -1, 1 };
-
-		Queue<int[]> queue = new LinkedList<>();
-
-		// 익은 토마토의 위치를 큐에 담아준다.
-		for (int i = 0; i < map.length; i++) {
-			for (int j = 0; j < map[i].length; j++) {
-				if (map[i][j] == 1) {
-					queue.add(new int[] { i, j });
-				}
-			}
+	private static boolean isChecked(int r, int c) {
+		if (r >= 0 && r < N && c >= 0 && c < M) {
+			return true;
 		}
-
-		// 토마토 익을 때까지의 날짜 구하기 위한 연산
-		while (!queue.isEmpty()) {
-			int[] cLocation = queue.poll();
-
-			for (int i = 0; i < 4; i++) {
-				int nx = cLocation[0] + dx[i];
-				int ny = cLocation[1] + dy[i];
-
-				if (nx >= 0 && ny >= 0 && nx < map.length && ny < map[0].length && map[nx][ny] == 0) {
-					map[nx][ny] = map[cLocation[0]][cLocation[1]] + 1;
-					queue.add(new int[] { nx, ny });
-				}
-			}
-		}
-
-		int result = 0;
-
-		for (int i = 0; i < map.length; i++) {
-			for (int j = 0; j < map[i].length; j++) {
-				// 0이 있는 경우 토마토가 익지 않은 것이기에 -1 출력후 종료
-				if (map[i][j] == 0) {
-					return -1;
-				}
-				// 최댓값 계산
-				result = Math.max(result, map[i][j]);
-			}
-		}
-
-		// 기다린 날짜는 최댓값에서 -1
-		return result - 1;
+		return false;
 	}
 }
