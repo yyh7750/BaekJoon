6febc3dba980d3fb69c2e67bcc2fef5f914b2b64
Younghun Yoo <66967395+yyh7750@users.noreply.github.com>
2022-10-05 14:17:48 +0900

    [Gold IV] Title: 벽 부수고 이동하기, Time: 708 ms, Memory: 162700 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -5,107 +5,91 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-/**
- * @packageName : 단계별_문제풀이.너비우선정렬
- * @description : n*m 행렬에서 벽을 한번만 부술 수 있는 경우 끝지점에 도달하는 최솟값을 구하는 프로그램. bfs 활용
- * @author : Younghun Yu
- * @date : 2022.05.16
- * ===========================================================
- *     DATE      AUTHOR      NOTE
- * -----------------------------------------------------------
- * 2022.05.16  Younghun Yu  최초 생성
- */
 public class Main {
+
+	static class Loc {
+		int r;
+		int c;
+		int cnt;
+		int chance; // 1이면 벽 부수고 이동 가능, 0이면 불가능
+
+		public Loc(int r, int c, int cnt, int chance) {
+			super();
+			this.r = r;
+			this.c = c;
+			this.cnt = cnt;
+			this.chance = chance;
+		}
+	}
+
+	private static int N, M, ans = Integer.MAX_VALUE;
+	private static int[][] map;
+	private static int[] dr = { 0, 1, 0, -1 };
+	private static int[] dc = { 1, 0, -1, 0 };
+	private static boolean[][][] visited;
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
 
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 
-		// 도착 지점이 시작 지점과 같을 경우의 처리. (1x1 행렬)
-		if (n - 1 == 0 && m - 1 == 0) {
-			sb.append(1);
-		} //
-		else {
-			// 좌우
-			int[] dx = { -1, 1, 0, 0 };
-			// 상하
-			int[] dy = { 0, 0, -1, 1 };
+		map = new int[N][M];
+		visited = new boolean[N][M][2];
 
-			// n*m 크기의 맵 배열
-			char[][] map = new char[n][m];
-			// 거리 측정 배열
-			int[][] dist = new int[n][m];
-			// 벽을 부순 여부에 따른 방문 기록 배열
-			boolean[][][] visited = new boolean[2][n][m];
-
-			Queue<int[]> queue = new LinkedList<>();
-
-			// map 입력
-			for (int i = 0; i < n; i++) {
-				String s = br.readLine();
-				for (int j = 0; j < m; j++) {
-					map[i][j] = s.charAt(j);
-				}
+		for (int i = 0; i < N; i++) {
+			char[] inputArr = br.readLine().toCharArray();
+			for (int j = 0; j < M; j++) {
+				map[i][j] = inputArr[j] - '0';
 			}
+		}
 
-			// 시작점 : x, y, 벽 부쉈는지 여부
-			queue.offer(new int[] { 0, 0, 0 });
+		bfs(0, 0);
 
-			loop : while (!queue.isEmpty()) {
-				// 현재 위치
-				int[] curLocation = queue.poll();
+		br.close();
+		ans = ans == Integer.MAX_VALUE ? -1 : ans;
+		System.out.println(ans);
+	}
 
-				// 상, 하, 좌, 우 검사
-				for (int i = 0; i < 4; i++) {
-					int nx = curLocation[0] + dx[i];
-					int ny = curLocation[1] + dy[i];
+	private static void bfs(int sr, int sc) {
+		Queue<Loc> Q = new LinkedList<>();
+		Q.offer(new Loc(sr, sc, 1, 1));
+		visited[sr][sc][1] = true;
 
-					if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
-						continue;
-					}
+		while (!Q.isEmpty()) {
+			Loc cur = Q.poll();
 
-					// 다음 칸에 벽이 있을 때
-					if (map[nx][ny] == '1') {
-						// 벽을 부순적이 있는지 체크, 벽을 방문한적이 있는지 체크
-						if (curLocation[2] == 0 && !visited[1][nx][ny]) {
-							visited[curLocation[2]][nx][ny] = true; // 방문 처리
-							dist[nx][ny] = dist[curLocation[0]][curLocation[1]] + 1; // 거리 측정
-							queue.offer(new int[] { nx, ny, 1 }); // 다시 큐에 넣어줘서 BFS!
-						}
-					}
-					// 벽이 아닐 경우
-					else {
-						// 방문하지 않은 경우
-						if (!visited[curLocation[2]][nx][ny]) {
-							// 방문 처리
-							visited[curLocation[2]][nx][ny] = true;
-							// 거리 측정
-							dist[nx][ny] = dist[curLocation[0]][curLocation[1]] + 1;
-							// bfs 반복을 위한 큐에 삽입
-							queue.offer(new int[] { nx, ny, curLocation[2] });
-						}
+			for (int d = 0; d < 4; d++) {
+				int nr = cur.r + dr[d];
+				int nc = cur.c + dc[d];
+
+				if (isChecked(nr, nc)) {
+					// 벽을 안부수고 가는 경우
+					if(map[nr][nc] == 0 && !visited[nr][nc][cur.chance]) {
+						Q.offer(new Loc(nr, nc, cur.cnt+1, cur.chance));
+						visited[nr][nc][cur.chance] = true;
 					}
 					
-					// 도착
-					if (nx == n - 1 && ny == m - 1) {
-						sb.append(dist[nx][ny] + 1);
-                        break loop;
+					// 벽을 부수고 가는 경우
+					if(map[nr][nc] == 1 && cur.chance == 1) {
+						Q.offer(new Loc(nr, nc, cur.cnt+1, 0));
+						visited[nr][nc][0] = true;
 					}
 				}
 			}
-		}
-        br.close();
 
-		// 도착하지 못했을 경우
-		if (sb.length() == 0) {
-			sb.append(-1);
+			if (cur.r == N - 1 && cur.c == M - 1) {
+				ans = Math.min(ans, cur.cnt);
+			}
 		}
-			
-		System.out.println(sb);
+	}
+
+	private static boolean isChecked(int nr, int nc) {
+		if (nr >= 0 && nr < N && nc >= 0 && nc < M) {
+			return true;
+		}
+		return false;
 	}
 }
