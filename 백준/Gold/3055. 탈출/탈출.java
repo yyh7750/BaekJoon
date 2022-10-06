b6ef90cd75ac6c77675cfa161277f7d97e3a6b41
Younghun Yoo <66967395+yyh7750@users.noreply.github.com>
2022-10-07 00:11:33 +0900

    [Gold IV] Title: 탈출, Time: 84 ms, Memory: 11996 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.java
+++ 백준/Gold/3055. 탈출/탈출.java
@@ -1,113 +1,118 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+// bfs Q 하나만 이용하여 풀어보기
 public class Main {
 
 	static class Loc {
 		int r;
 		int c;
 		int cnt;
+		char type; // 물인지, 고슴도치인지
 
-		public Loc(int r, int c, int cnt) {
+		public Loc(int r, int c, int cnt, char type) {
 			super();
 			this.r = r;
 			this.c = c;
 			this.cnt = cnt;
+			this.type = type;
 		}
 	}
 
-	private static int R, C, result = Integer.MIN_VALUE;
+	private static int N, M;
 	private static char[][] map;
-	private static int[] dr = { -1, 1, 0, 0 };
-	private static int[] dc = { 0, 0, -1, 1 };
-	private static Queue<Loc> amurQ, waterQ;
-	private static boolean[][] amurVisited;
-	private static boolean[][] waterVisited;
+	private static boolean[][] visited;
+	private static int[] dr = { 1, -1, 0, 0 };
+	private static int[] dc = { 0, 0, 1, -1 };
+	private static ArrayList<Loc> waterList;
+	private static boolean flag;
+	private static String ans;
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st = null;
 
-		R = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		map = new char[R][C];
-		amurQ = new LinkedList<>();
-		waterQ = new LinkedList<>();
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		map = new char[N][M];
+		visited = new boolean[N][M];
+		waterList = new ArrayList<>();
 
-		for (int i = 0; i < R; i++) {
-			String row = br.readLine();
-			for (int j = 0; j < C; j++) {
-				map[i][j] = row.charAt(j);
+		int sr = 0, sc = 0;
+
+		for (int i = 0; i < N; i++) {
+			String input = br.readLine();
+			for (int j = 0; j < M; j++) {
+				map[i][j] = input.charAt(j);
 
 				if (map[i][j] == 'S') {
-					amurQ.offer(new Loc(i, j, 0));
+					sr = i;
+					sc = j;
 				} //
 				else if (map[i][j] == '*') {
-					waterQ.offer(new Loc(i, j, 0));
+					waterList.add(new Loc(i, j, 0, '*'));
 				}
 			}
 		}
 
-		amurBfs();
+		flag = false;
+		bfs(sr, sc);
 
-		System.out.println(result == Integer.MIN_VALUE ? "KAKTUS" : result);
+		br.close();
+		System.out.println(ans);
 	}
 
-	private static void amurBfs() {
-		amurVisited = new boolean[R][C];
+	private static void bfs(int sr, int sc) {
+		Queue<Loc> Q = new LinkedList<>();
 
-		while (!amurQ.isEmpty()) {
-			waterBfs();
-			int qSize = amurQ.size();
-
-			for (int i = 0; i < qSize; i++) {
-				Loc cur = amurQ.poll();
-
-				if (map[cur.r][cur.c] == 'D') {
-					result = cur.cnt;
-				}
-
-				for (int d = 0; d < 4; d++) {
-					int nr = cur.r + dr[d];
-					int nc = cur.c + dc[d];
-
-					if (isChecked(nr, nc) && !amurVisited[nr][nc] && map[nr][nc] != '*' && map[nr][nc] != 'X') {
-						amurVisited[nr][nc] = true;
-						amurQ.offer(new Loc(nr, nc, cur.cnt + 1));
-					}
-				}
-			}
+		for (Loc water : waterList) {
+			Q.offer(water);
 		}
-	}
+		Q.offer(new Loc(sr, sc, 0, 'S'));
 
-	private static void waterBfs() {
-		waterVisited = new boolean[R][C];
+		while (!Q.isEmpty()) {
+			Loc cur = Q.poll();
 
-		int qSize = waterQ.size();
-
-		for (int i = 0; i < qSize; i++) {
-			Loc cur = waterQ.poll();
+			if (cur.type == 'S' && map[cur.r][cur.c] == 'D') {
+				ans = cur.cnt + "";
+				flag = true;
+				break;
+			}
 
 			for (int d = 0; d < 4; d++) {
 				int nr = cur.r + dr[d];
 				int nc = cur.c + dc[d];
 
-				if (isChecked(nr, nc) && !waterVisited[nr][nc] && map[nr][nc] == '.') {
-					waterVisited[nr][nc] = true;
-					map[nr][nc] = '*';
-					waterQ.offer(new Loc(nr, nc, 0));
+				if (isChecked(nr, nc) && map[nr][nc] != 'X') {
+					// 악마일 경우
+					if (cur.type == '*' && map[nr][nc] != 'D' && map[nr][nc] != '*') {
+						map[nr][nc] = '*';
+						Q.offer(new Loc(nr, nc, 0, '*'));
+					}
+
+					// 수현이일 경우
+					if (cur.type == 'S' && map[nr][nc] != '*' && !visited[nr][nc]) {
+						visited[nr][nc] = true;
+						Q.offer(new Loc(nr, nc, cur.cnt + 1, 'S'));
+					}
 				}
 			}
 		}
+
+		if (!flag) {
+			ans = "KAKTUS";
+		}
 	}
 
-	private static boolean isChecked(int r, int c) {
-		if (r >= 0 && r < R && c >= 0 && c < C) {
+	private static boolean isChecked(int nr, int nc) {
+		if (nr >= 0 && nr < N && nc >= 0 && nc < M) {
 			return true;
 		}
 		return false;
