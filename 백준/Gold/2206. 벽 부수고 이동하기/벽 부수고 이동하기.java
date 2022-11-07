b6fc9f37dc8ffd47ea12b358b86800ba69cfea79
Younghun Yoo <66967395+yyh7750@users.noreply.github.com>
2022-11-08 00:37:09 +0900

    [Gold IV] Title: 벽 부수고 이동하기, Time: 700 ms, Memory: 160432 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -8,10 +8,7 @@ import java.util.StringTokenizer;
 public class Main {
 
 	static class Loc {
-		int r;
-		int c;
-		int cnt;
-		int chance; // 1이면 벽 부수고 이동 가능, 0이면 불가능
+		int r, c, cnt, chance;
 
 		public Loc(int r, int c, int cnt, int chance) {
 			super();
@@ -22,11 +19,12 @@ public class Main {
 		}
 	}
 
-	private static int N, M, ans = Integer.MAX_VALUE;
+	private static int N, M;
 	private static int[][] map;
-	private static int[] dr = { 0, 1, 0, -1 };
-	private static int[] dc = { 1, 0, -1, 0 };
 	private static boolean[][][] visited;
+	private static int[] dr = { -1, 1, 0, 0 };
+	private static int[] dc = { 0, 0, -1, 1 };
+	private static int Ans = Integer.MAX_VALUE;
 
 	public static void main(String[] args) throws IOException {
 
@@ -35,28 +33,28 @@ public class Main {
 
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-
 		map = new int[N][M];
 		visited = new boolean[N][M][2];
 
 		for (int i = 0; i < N; i++) {
-			char[] inputArr = br.readLine().toCharArray();
+			String str = br.readLine();
 			for (int j = 0; j < M; j++) {
-				map[i][j] = inputArr[j] - '0';
+				map[i][j] = str.charAt(j) - '0';
 			}
 		}
 
 		bfs(0, 0);
 
 		br.close();
-		ans = ans == Integer.MAX_VALUE ? -1 : ans;
-		System.out.println(ans);
+		System.out.println(Ans == Integer.MAX_VALUE ? -1 : Ans);
 	}
 
-	private static void bfs(int sr, int sc) {
+	// 벽을 부쉈는지 안부쉈는지에 대한 처리가 필요.
+	// -> 3차원 방문배열을 이용하여 cur.chance를 기준으로 벽을 부순 상태인지 아닌지에 대한 처리를 해준다.
+	private static void bfs(int r, int c) {
 		Queue<Loc> Q = new LinkedList<>();
-		Q.offer(new Loc(sr, sc, 1, 1));
-		visited[sr][sc][1] = true;
+		Q.offer(new Loc(r, c, 1, 1));
+		visited[r][c][1] = true; // 찬스가 남아 있는 경우. (벽을 부수지 않은 상태)
 
 		while (!Q.isEmpty()) {
 			Loc cur = Q.poll();
@@ -66,28 +64,33 @@ public class Main {
 				int nc = cur.c + dc[d];
 
 				if (isChecked(nr, nc)) {
-					// 벽을 안부수고 가는 경우
-					if(map[nr][nc] == 0 && !visited[nr][nc][cur.chance]) {
-						Q.offer(new Loc(nr, nc, cur.cnt+1, cur.chance));
-						visited[nr][nc][cur.chance] = true;
+					// 벽일때
+					if (map[nr][nc] == 1) {
+						// 부술 수 있다면 부수고 간다.
+						if (cur.chance == 1) {
+							visited[nr][nc][0] = true;
+							Q.offer(new Loc(nr, nc, cur.cnt + 1, 0));
+						}
 					}
-					
-					// 벽을 부수고 가는 경우
-					if(map[nr][nc] == 1 && cur.chance == 1) {
-						Q.offer(new Loc(nr, nc, cur.cnt+1, 0));
-						visited[nr][nc][0] = true;
+					// map[nr][nc] == 0 빈 공간일 경우 그냥 간다.
+					else {
+						if (!visited[nr][nc][cur.chance]) {
+							visited[nr][nc][cur.chance] = true;
+							Q.offer(new Loc(nr, nc, cur.cnt + 1, cur.chance));
+						}
 					}
 				}
 			}
-
+			
 			if (cur.r == N - 1 && cur.c == M - 1) {
-				ans = Math.min(ans, cur.cnt);
+				Ans = cur.cnt;
+				return;
 			}
 		}
 	}
 
 	private static boolean isChecked(int nr, int nc) {
-		if (nr >= 0 && nr < N && nc >= 0 && nc < M) {
+		if (nr >= 0 && nc >= 0 && nr < N && nc < M) {
 			return true;
 		}
 		return false;
